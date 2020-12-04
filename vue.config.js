62f4b65d06502d9897b4352dc48b69aab8c8a889
Noel Daz Jr <nmdazgames@gmail.com>
2020-12-04 22:30:35 +0800

    Fix Deploy.sh for Github Pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/elearning-frontend/' : '/',
+	publicPath: process.env.NODE_ENV === 'production' ? '/elearning/' : '/',
 	configureWebpack: {
 		devtool: 'source-map'
 	}
