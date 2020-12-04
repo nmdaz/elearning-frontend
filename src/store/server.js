93660e436fae2ff2b66ecccafebc682e6ec0c5fe
Noel Daz Jr <nmdazgames@gmail.com>
2020-12-04 22:30:35 +0800

    Fix Deploy.sh for Github Pages

--- src/store/server.js
+++ src/store/server.js
@@ -1,7 +1,7 @@
 export default {
 	state: {
-		url: 'http://elearning.test',
-		apiUrl: 'http://elearning.test/api'
+		url: 'https://elearning.test',
+		apiUrl: 'https://elearning.test/api'
 		// url: 'https://evening-river-03008.herokuapp.com/',
 		// apiUrl: 'https://evening-river-03008.herokuapp.com/api'
 	}
