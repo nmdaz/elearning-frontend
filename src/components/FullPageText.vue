b5f82becc276b06126f6c672705ce13268bd9161
Noel Daz Jr <nmdazgames@gmail.com>
2020-12-05 22:34:29 +0800

    Fix Frontend for backend integration

---
+++ src/components/FullPageText.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="full-page-text">
+		<div>{{ text }}</div>
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'FullPageText',
+	props: [ 'text' ]
+}	
+
+</script>
+
+<style lang="scss">
+
+.full-page-text {
+	font-size: 1rem;
+	position: absolute;
+	height: 50vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+}
+
+</style>
