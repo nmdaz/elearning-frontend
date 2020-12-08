1945b19bc1c4f9ae793880eb773de9fc860c7fef
Noel Daz Jr <nmdazgames@gmail.com>
2020-12-08 22:05:50 +0800

    Improve Folder Structure

---
+++ src/components/controls/BaseButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="base-button" :type="type" @click="$emit('click')">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'BaseButton',
+        props: { type: String }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .base-button {
+        padding: 0.6rem 1.5rem;
+        background-color: #38bb8e;
+        border: none;
+        letter-spacing: 0.12rem;
+        font-size: 0.75rem;
+        cursor: pointer;
+        color: white;
+        font-family: Roboto;
+        text-transform: uppercase;
+    }
+</style>
---
+++ src/components/controls/BaseForm.vue
@@ -0,0 +1,40 @@
+<template>
+    <form class="basic-form" @submit.prevent="$emit('submit', $event.target.value)">
+        <legend v-if="legend" class="basic-form__legend">{{ legend }}</legend>
+        <div class="basic-form__body">
+            <slot></slot>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'BaseForm',
+    props: [ 'legend' ]
+}
+</script>
+
+<style lang="scss" scoped>
+.basic-form {
+    max-width: 500px;
+    margin: auto;
+
+    &__legend {
+        padding: 1rem 0.75rem;
+        color: white;
+        font-size: 0.75rem;
+        font-weight: bold;
+        letter-spacing: 0.05rem;
+        text-align: center;
+        color: #555;
+        font-size: 1.5rem;
+        letter-spacing: 0.05rem;
+        margin-bottom: 1.5rem;
+        font-family: Roboto;
+    }
+
+    &__body {
+        padding: 0 2rem 2rem 2rem;
+    }
+}
+</style>
